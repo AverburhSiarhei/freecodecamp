8d9c7d11a4a3e462176a8e4459af4a9713992836
Siarhey Averburh <Averbyrg@gmail.com>
2024-11-24 15:35:24 +0300

    Step 8
    BuildingPyramidGenerator
    
    add solution

--- JS/BuildingPyramidGenerator/Step 8.js
+++ JS/BuildingPyramidGenerator/Step 8.js
@@ -9,3 +9,11 @@
 // let yetAnotherVariable;
 // let thisIsAnAbsurdlyLongName;
 // Use camel case to declare a new secondCharacter variable.
+
+// Solution
+
+let character = 'Hello';
+console.log(character);
+character = "World";
+console.log(character);
+let secondCharacter;
